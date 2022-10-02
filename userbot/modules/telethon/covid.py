e5c6f36f6b722dd3c25acba9720399044c277732
Sip - userbot <110762610+sip-userbot@users.noreply.github.com>
2022-10-02 22:17:52 +0700

    Update covid.py

--- userbot/modules/telethon/covid.py
+++ userbot/modules/telethon/covid.py
@@ -5,7 +5,7 @@ from . import nandeub
 plugin_category = "plugins"
 
 
-@nandeub.sip-userbot_cmd(
+@nandeub.sipuserbot_cmd(
     pattern="covid(?: |$)(.*)",
     command=("covid", plugin_category),
     info={
