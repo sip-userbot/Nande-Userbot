3cae420df44016588ecfb408d42dc1c23c38e4e3
Sip - userbot <110762610+sip-userbot@users.noreply.github.com>
2022-10-02 22:45:39 +0700

    Update sudo.py

--- userbot/modules/telethon/sudo.py
+++ userbot/modules/telethon/sudo.py
@@ -33,7 +33,7 @@ def get_key(val):
     return None
 
 
-@nandeub.sip-userbot_cmd(
+@nandeub.sipuserbot_cmd(
     pattern="sudo (on|off)$",
     command=("sudo", plugin_category),
     info={
@@ -87,7 +87,7 @@ async def chat_blacklist(event):
     await edit_delete(event, "It was turned off already")
 
 
-@nandeub.sip-userbot_cmd(
+@nandeub.sipuserbot_cmd(
     pattern="addsudo(?: |$)(.*)",
     command=("addsudo", plugin_category),
     info={
@@ -127,7 +127,7 @@ async def add_sudo_user(event):
     await event.client.reload(msg)
 
 
-@nandeub.sip-userbot_cmd(
+@nandeub.sipuserbot_cmd(
     pattern="delsudo(?: |$)(.*)",
     command=("delsudo", plugin_category),
     info={
@@ -158,7 +158,7 @@ async def _(event):
     await event.client.reload(msg)
 
 
-@nandeub.sip-userbot_cmd(
+@nandeub.sipuserbot_cmd(
     pattern="vsudo$",
     command=("vsudo", plugin_category),
     info={
@@ -187,7 +187,7 @@ async def _(event):
     await edit_or_reply(event, result)
 
 
-@nandeub.sip-userbot_cmd(
+@nandeub.sipuserbot_cmd(
     pattern="addscmd(s)? ((.|\n)*)",
     command=("addscmd", plugin_category),
     info={
@@ -286,7 +286,7 @@ async def _(event):  # sourcery no-metrics
     await event.client.reload(msg)
 
 
-@nandeub.sip-userbot_cmd(
+@nandeub.sipuserbot_cmd(
     pattern="rmscmd(s)? ((.|\n)*)?",
     command=("rmscmd", plugin_category),
     info={
@@ -380,7 +380,7 @@ async def _(event):  # sourcery no-metrics
     await event.client.reload(msg)
 
 
-@nandeub.sip-userbot_cmd(
+@nandeub.sipuserbot_cmd(
     pattern="vscmds( -d)?$",
     command=("vscmds", plugin_category),
     info={
